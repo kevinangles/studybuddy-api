2cac1be73e5362f79df34d3b698ea8c1351f8093
Kevin <kangl010@fiu.edu>
2018-12-12 14:34:05 -0500

    Add dialectOptions to Sequelize connection

--- config/db.js
+++ config/db.js
@@ -5,6 +5,9 @@ const sequelize = new Sequelize(env.DATABASE_NAME, env.DATABASE_USERNAME, env.DA
   host: env.DATABASE_HOST,
   port: env.DATABASE_PORT,
   dialect: 'postgres',
+  dialectOptions: {
+    ssl:'Amazon RDS'
+  },
   operatorsAliases: Sequelize.Op,
   define: {
     underscored: true

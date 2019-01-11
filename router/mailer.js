c8e0d608abfea41ba2cb6d87272b787ae0b3ed5b
Kevin <kangl010@fiu.edu>
2019-01-11 15:18:26 -0500

    Import environment variable for email credentials

--- router/mailer.js
+++ router/mailer.js
@@ -1,22 +1,17 @@
+const env = require('./../config');
+
 const nodemailer = require("nodemailer");
 
 let mailer = {
   transporter: nodemailer.createTransport({
     host: 'smtp.domain.com',
     port: 465,
-    secure: true, // use SSL
+    secure: true,
     auth: {
-      user: 'noreply@studybuddy.coffee', // generated ethereal user
-      pass: 'wzQjK6VJmM9c8cSE' // generated ethereal password
+      user: env.NOREPLY_EMAIL_ADDRESS,
+      pass: env.NOREPLY_EMAIL_PASSWORD
     }
-  }),
-  mailOptions: {
-    from: '"Studybuddy" <noreply@studybuddy.coffee>', // sender address
-    to: "kevinangles@gmail.com", // list of receivers
-    subject: "Studybuddy Email Verification! 🤓", // Subject line
-    // text: "Hi! Please, confirm your email.", // plain text body
-    html: "<b>Hello world?</b>" // html body
-  }
+  })
 };
 
 module.exports = mailer;

184e2506a1f55515eb2bc5e700fe9b904f2a663e
Kevin <kangl010@fiu.edu>
2019-01-17 12:57:00 -0500

    Clean up register route by moving email message to mailer file

--- router/routes/users.js
+++ router/routes/users.js
@@ -51,25 +51,7 @@ module.exports = (app, db) => {
                   from: '"Studybuddy" <noreply@studybuddy.coffee>',
                   to: newUser.email,
                   subject: "Verify your Email Address",
-                  html: `
-                    <div style="width: 550px; background-color: #ffcc00; padding: 15px; margin: 0px auto;">
-                      <div style="width: 500px; background-color: #fff; padding: 25px; font-size: 18px;">
-                        <div style="width: 500px; text-align: center;">
-                          <img src="https://res.cloudinary.com/studybuddy/image/upload/c_scale,q_100,w_350/v1547215183/logo.png">
-                        </div><br>
-                        Hi, ${newUser.first_name}!<br>
-                        <br>
-                        Thank you for joining Studybuddy! We're really excited to welcome you into our community of students from Florida International University. Before you start connecting with your peers, we need you to take a second to <b>verify your email address by clicking on the button below</b>:<br>
-                        <br>
-                        <a style="width: 175px; height: 60px; text-decoration: none; margin: 0px auto; background-color: #ffcc00; color: #fff; text-align: center; line-height: 60px; display: block;" href="${url}">Verify email</a>
-                        <br>
-                        Thanks again for joining 🤓<br>
-                        <br>
-                        Kevin Angles Belledonne<br>
-                        Founder, Studybuddy<br>
-                      </div>
-                    </div>
-                    `
+                  html: mailer.html(newUser.first_name, url),
                 }, (error, info) => {
                   if (error) { return console.log(error); }
                   console.log("Message sent: %s", info.messageId);

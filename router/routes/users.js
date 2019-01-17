7e22995a5f2bd02093e3c4b71b03a52df3d03308
Kevin <kangl010@fiu.edu>
2019-01-17 12:53:36 -0500

    Add error handling for edge cases when loging in and registering

--- router/routes/users.js
+++ router/routes/users.js
@@ -4,7 +4,7 @@ const mailer = require('./../mailer');
 const bcrypt = require('bcrypt');
 const crypto = require('crypto');
 
-const sanitizePhoneNumber = phone_number => phone_number.replace(/\D/g,'');
+const sanitizePhoneNumber = phone_number => phone_number.replace(/\D/g, '');
 
 const verifyEmailDomain = email => {
   const domain = 'fiu.edu';
@@ -22,38 +22,36 @@ module.exports = (app, db) => {
     const phone_number = sanitizePhoneNumber(req.body.phone_number);
     const references = req.body.references;
 
-    if(!verifyEmailDomain(email)) { return res.status(409).send({ message: 'Email must be valid FIU email' }); }
+    if (!verifyEmailDomain(email)) { return res.status(409).send({ message: 'Email must be valid FIU email' }); }
 
     db.users.findOne({
       where: { email: email }
     })
       .then(existingUser => {
-        if (existingUser) {
-          return res.status(409).send({ message: 'The email ' + existingUser.email + ' is taken' });
-        } else {
-          db.users.create({
-            first_name: first_name,
-            last_name: last_name,
-            email: email,
-            password: password,
-            phone_number: phone_number
-          })
-            .then(newUser => {
-              for (reference of references) {
-                newUser.addCourse(reference);
-              }
-              const hash = crypto.randomBytes(20).toString('hex');
-              db.emailHashes.create({
-                uuid: newUser.uuid,
-                hash: hash
-              })
-                .then(readyUser => {
-                  let url = `https://www.studybuddy.coffee/verify/${readyUser.hash}`;
-                  mailer.transporter.sendMail({
-                    from: '"Studybuddy" <noreply@studybuddy.coffee>',
-                    to: newUser.email,
-                    subject: "Verify your Email Address",
-                    html: `
+        if (existingUser) { return res.status(409).send({ message: 'Email ' + existingUser.email + ' is already taken' }); }
+        db.users.create({
+          first_name: first_name,
+          last_name: last_name,
+          email: email,
+          password: password,
+          phone_number: phone_number
+        })
+          .then(newUser => {
+            for (reference of references) {
+              newUser.addCourse(reference);
+            }
+            const hash = crypto.randomBytes(20).toString('hex');
+            db.emailHashes.create({
+              uuid: newUser.uuid,
+              hash: hash
+            })
+              .then(readyUser => {
+                let url = `https://www.studybuddy.coffee/verify/${readyUser.hash}`;
+                mailer.transporter.sendMail({
+                  from: '"Studybuddy" <noreply@studybuddy.coffee>',
+                  to: newUser.email,
+                  subject: "Verify your Email Address",
+                  html: `
                     <div style="width: 550px; background-color: #ffcc00; padding: 15px; margin: 0px auto;">
                       <div style="width: 500px; background-color: #fff; padding: 25px; font-size: 18px;">
                         <div style="width: 500px; text-align: center;">
@@ -72,16 +70,15 @@ module.exports = (app, db) => {
                       </div>
                     </div>
                     `
-                  }, (error, info) => {
-                    if (error) { return console.log(error); }
-                    console.log("Message sent: %s", info.messageId);
-                  });
-                  auth.generateToken(res, readyUser);
-                })
-                .catch(next);
-            })
-            .catch(next);
-        }
+                }, (error, info) => {
+                  if (error) { return console.log(error); }
+                  console.log("Message sent: %s", info.messageId);
+                });
+                auth.generateToken(res, readyUser);
+              })
+              .catch(next);
+          })
+          .catch(next);
       })
       .catch(next)
   });
@@ -96,8 +93,10 @@ module.exports = (app, db) => {
       attributes: { exclude: ['id'] }
     })
       .then(foundUser => {
+        if (!foundUser) { return res.status(409).send({ message: 'Email is invalid' }); }
         bcrypt.compare(password, foundUser.password).then(match => {
-          if (match) { auth.generateToken(res, foundUser); }
+          if (!match) { return res.status(409).send({ message: 'Password is invalid' }); }
+          auth.generateToken(res, foundUser);
         });
       })
       .catch(next);
@@ -110,6 +109,7 @@ module.exports = (app, db) => {
       where: { hash: hash }
     })
       .then(foundUUID => {
+        if (!foundUUID) { return res.status(409).send({ message: 'Email verification link expired or is invalid' }); }
         foundUUID.destroy();
         db.users.update({ email_verified: true }, {
           where: { uuid: foundUUID.uuid },

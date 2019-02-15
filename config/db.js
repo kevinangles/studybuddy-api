4982ace7a4fd23f0a5c75520ba85df93ecc9c330
Kevin <kangl010@fiu.edu>
2019-02-15 16:35:19 -0500

    Updated config files with proper ESLint patterns

--- config/db.js
+++ config/db.js
@@ -6,8 +6,8 @@ const sequelize = new Sequelize(env.DATABASE_NAME, env.DATABASE_USERNAME, env.DA
   dialect: env.DATABASE_DIALECT,
   operatorsAliases: Sequelize.Op,
   define: {
-    underscored: true
-  }
+    underscored: true,
+  },
 });
 
 // Connect all the models in the database to a db object,
@@ -21,9 +21,15 @@ db.sequelize = sequelize;
 db.users = require('../models/users.js')(sequelize, Sequelize);
 db.courses = require('../models/courses.js')(sequelize, Sequelize);
 db.emailHashes = require('../models/emailHashes')(sequelize, Sequelize);
+db.user_courses = require('../models/user_courses')(sequelize, Sequelize);
+db.professors = require('../models/professors')(sequelize, Sequelize);
 
 // Relations
-db.users.belongsToMany(db.courses, {through: 'users_courses'});
-db.courses.belongsToMany(db.users, {through: 'users_courses'});
+// db.users.belongsToMany(db.courses, {through: 'users_courses'});
+// db.courses.belongsToMany(db.users, {through: 'users_courses', otherKey: 'code'});
+db.professors.hasMany(db.courses, { foreignKey: 'id' });
+db.courses.belongsTo(db.professors, { as: 'professor_data', foreignKey: 'id' });
+db.users.belongsToMany(db.courses, { as: 'classes', through: 'user_courses' });
+db.courses.belongsToMany(db.users, { as: 'students', through: 'user_courses' });
 
 module.exports = db;
--- config/index.js
+++ config/index.js
@@ -2,7 +2,7 @@ const config = {
   dev: 'development',
   test: 'testing',
   prod: 'production',
-  port: process.env.PORT || 3000
+  port: process.env.PORT || 3000,
 };
 
 process.env.NODE_ENV = process.env.NODE_ENV || config.dev;
@@ -13,7 +13,7 @@ let envConfig;
 try {
   envConfig = require('./' + config.env);
   envConfig = envConfig || {};
-} catch(e) {
+} catch (e) {
   envConfig = {};
 }
 
